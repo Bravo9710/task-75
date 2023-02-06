9b457af5bcd4b6571f5be5657ab3e0a505ac4ae9
vvenkov <vvenkov@htmlbox.net>
2023-02-06 20:48:38 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [text, setText] = useState();
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+    localStorage.setItem('text', text);
+  }
+
+  const handleClear = (e) => {
+    setText('');
+  }
+
+  useEffect(() => {
+    const storedText = localStorage.getItem('text');
+    if (storedText) {
+      setText(storedText);
+    }
+  }, []);
+
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
+            <textarea onChange={handleChange} value={text} className="textarea is-large" placeholder="Notes..." />
           </div>
         </div>
         <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
+        <button onClick={handleClear} className="button is-large">Clear</button>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./index.css";
 
 import App from "./App";
 import React from "react";
+
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 
