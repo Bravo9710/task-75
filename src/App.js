0a862abd73e519ec29750b010887a50cc10b2897
vvenkov <vvenkov@htmlbox.net>
2023-02-06 21:20:09 +0200

    done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [text, setText] = useState();
+  const [text, setText] = useState("");
 
   const handleChange = (e) => {
     setText(e.target.value);
-    localStorage.setItem('text', text);
   }
 
   const handleClear = (e) => {
     setText('');
+  }
+
+  const handleSave = (e) => {
+    localStorage.setItem('text', text);
   }
 
   useEffect(() => {
@@ -28,7 +31,7 @@ function App() {
             <textarea onChange={handleChange} value={text} className="textarea is-large" placeholder="Notes..." />
           </div>
         </div>
-        <button className="button is-large is-primary is-active">Save</button>
+        <button onClick={handleSave} className="button is-large is-primary is-active">Save</button>
         <button onClick={handleClear} className="button is-large">Clear</button>
       </div>
     </div>
